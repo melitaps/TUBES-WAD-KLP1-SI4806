29a9d539d599cad97c63230fee83925c82cbb037
melitaps <pramasundarimelita@gmail.com>
2025-12-26 23:21:05 +0700

    progres

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,26 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+/**
+ * ==========1===========
+ * unprotected routes for user registration and login
+ */
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+/**
+ * =========2===========
+ * protected routes, only accessible with valid token
+ */
+Route::middleware('auth:sanctum')->group(function () {
+    /**
+     * =========3===========
+     * User logout route
+     */
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+
+
+});
