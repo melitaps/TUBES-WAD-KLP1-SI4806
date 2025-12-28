cc62062ffe64ec07e9a30f5365124826cdf525b0
ffiraaa <anindyaprattista@gmail.com>
2025-12-28 14:35:49 +0700

    Progress 40%

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
+<<<<<<< Updated upstream
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+=======
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MenuController;
+
+Route::get('/menu', [MenuController::class, 'index']);
+Route::post('/menu', [MenuController::class, 'store']);
+Route::get('/menu/{id}', [MenuController::class, 'show']);
+Route::put('/menu/{id}', [MenuController::class, 'update']);
+Route::delete('/menu/{id}', [MenuController::class, 'destroy']);
+>>>>>>> Stashed changes
