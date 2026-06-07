8aef41ec8030460d91edfddfa0bc752b667f4769
melitaps <pramasundarimelita@gmail.com>
2026-06-07 22:44:07 +0700

    fix before deploy

--- database/migrations/2025_12_28_054939_create_wilayah_table.php
+++ database/migrations/2025_12_28_054939_create_wilayah_table.php
@@ -11,6 +11,7 @@ return new class extends Migration {
             $table->id();
             $table->string('provinsi')->index();
             $table->string('kota_kabupaten')->index();
+            $table->timestamps();
         });
     }
 
